c4948dc298db00d0591987b2f7f61b503e250c89
Laura Gong <lkgong09@gmail.com>
2019-10-27 18:12:59 -0700

    Delete

---
+++ Arrays/Delete/array.h
@@ -0,0 +1,25 @@
+//
+//  array.h
+//  
+//
+//  Created by Laura Gong on 10/24/19.
+//
+
+#ifndef GUARD_array_h
+#define GUARD_array_h
+
+struct Array {
+    int *array;
+    int size; // size allocated
+    int length; // actual length of array
+};
+
+void Display(struct Array a);
+
+int Insert(struct Array *a, int index, int element);
+
+int Append(struct Array *a, int element);
+
+int Delete (struct Array *a, int index);
+
+#endif
