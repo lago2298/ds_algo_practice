4903051f32af7fb114342c4acbf67addccce929d
Laura Gong <lkgong09@gmail.com>
2019-10-31 16:51:07 -0700

    Added Little Endian/Big Endian test

---
+++ Bit Manipulation/LittleEndian_BigEndian.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//
+//  This program tells the user whether the machine is Little Endian (MSB first) or Big Endian
+//
+//
+
+#include <stdio.h>
+
+#define LITTLE_ENDIAN 1
+#define BIG_ENDIAN 0
+
+int test()
+{
+    // Little Endian would display it as 0x62 0x61, Big Endian would display as 0x61 0x62
+    int x = 0x6162;  // 0x61 = 'a', 0x62 = 'b'
+    char *ptr_char = (char *)&x;
+    return *ptr_char == 'b' ? LITTLE_ENDIAN : BIG_ENDIAN;
+}
+
+int main()
+{
+    printf("Result of Test: %s\n", test() == LITTLE_ENDIAN ? "Little Endian" : "Big Endian");
+    return 0;
+}
