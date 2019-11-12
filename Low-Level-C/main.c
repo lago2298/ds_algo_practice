70c6175245d1abd2dcc86d58741f0e3a03d898fe
Laura Gong <lkgong09@gmail.com>
2019-11-12 09:45:53 -0800

    Added exapmle of non-string literal

--- Low-Level-C/main.c
+++ Low-Level-C/main.c
@@ -2,11 +2,13 @@
 
 extern int val;
 char *s = "hello";
+char t[] = "hello";
 
 int main()
 {
-	*(s+2) = 't';  // string literals are stored in read only memory?- so can't write to it
+	//*(s+2) = 't';  // string literals are stored in read only memory?- so can't write to it
+	t[3] = 'x';
 	printf("value: %d\n", val);
-	printf("string: %s", s);
+	printf("string: %s", t);
 	return 0;
 }
